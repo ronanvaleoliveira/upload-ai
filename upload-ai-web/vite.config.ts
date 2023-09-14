fbb1eac1239f85ad59a87d79cba7c885a9c533c1
Ronan <ronanvaleoliveira@gmail.com>
2023-09-13 21:12:23 -0300

    plugando o front com o back

--- upload-ai-web/vite.config.ts
+++ upload-ai-web/vite.config.ts
@@ -1,9 +1,12 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg','@ffmpeg/util']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
